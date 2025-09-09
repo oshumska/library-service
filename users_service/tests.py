12d3593796c6353b46eb8557b0bd0abcda654e6d
Olha Shumska <olgashumska65@gmail.com>
2025-09-09 08:30:31 +0200

    add: tests for unauthorized user

--- users_service/tests.py
+++ users_service/tests.py
@@ -5,6 +5,12 @@ from rest_framework import status
 from rest_framework.test import APIClient
 
 CREATE_USER_URL = reverse("users-service:register")
+USER_TOKEN_URL = reverse("users-service:token_obtain_pair")
+USER_ME_URL = reverse("users-service:manage-user")
+
+
+def sample_user(**params):
+    return get_user_model().objects.create_user(**params)
 
 
 class PublicUserServiceTests(TestCase):
@@ -23,3 +29,69 @@ class PublicUserServiceTests(TestCase):
         user = get_user_model().objects.get(**res.data)
         self.assertTrue(user.check_password(payload["password"]))
         self.assertNotIn("password", res.data)
+
+    def test_user_register_bad_request(self):
+        payload = {
+            "email": "username",
+            "password": "<PASSWORD>",
+        }
+
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        payload = {"email": "user@gmail.com", "password": "1234"}
+
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_user_already_exist(self):
+        payload = {
+            "email": "user@gmail.com",
+            "password": "<PASSWORD>",
+        }
+        sample_user(**payload)
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_user_create_jwt_tokens(self):
+        payload = {
+            "email": "user@gmail.com",
+            "password": "<PASSWORD>",
+        }
+
+        sample_user(**payload)
+        res = self.client.post(USER_TOKEN_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn("access", res.data)
+        self.assertIn("refresh", res.data)
+
+    def test_create_jwt_tokens_user_not_exist(self):
+        payload = {
+            "email": "user@gmail.com",
+            "password": "<PASSWORD>",
+        }
+
+        res = self.client.post(USER_TOKEN_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_jwt_tokens_no_password(self):
+        payload = {
+            "email": "user@gmail.com",
+            "password": "1234",
+        }
+        sample_user(**payload)
+        res = self.client.post(USER_TOKEN_URL, email=payload["email"])
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_jwt_tokens_wrong_password(self):
+        payload = {
+            "email": "user@gmail.com",
+            "password": "<PASSWORD>",
+        }
+        sample_user(**payload)
+        payload.update({"password": "wrong"})
+        res = self.client.post(USER_TOKEN_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_user_me_unauthorized(self):
+        res = self.client.get(USER_ME_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
