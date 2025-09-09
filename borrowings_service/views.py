2096ddfb1cb87ac1d024f125696cdb3df231c851
Olha Shumska <olgashumska65@gmail.com>
2025-09-09 16:04:42 +0200

    create: List Retrieve view for Borrowing

--- borrowings_service/views.py
+++ borrowings_service/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins, permissions
 
-# Create your views here.
+from borrowings_service.models import Borrowing
+from borrowings_service.serializers import BorrowingSerializer
+
+
+class BorrowingsListRetrieveAPIView(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+    permission_classes = (permissions.IsAuthenticated,)
