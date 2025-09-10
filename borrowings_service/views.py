62f2aac8262c3480fabaea80a12acf47f374df2c
Olha Shumska <olgashumska65@gmail.com>
2025-09-10 12:12:24 +0200

    modify view for borrowing so it will get serializer according to action and in create action will save user as current user

--- borrowings_service/views.py
+++ borrowings_service/views.py
@@ -1,12 +1,27 @@
 from rest_framework import viewsets, mixins, permissions
 
 from borrowings_service.models import Borrowing
-from borrowings_service.serializers import BorrowingSerializer
+from borrowings_service.serializers import (
+    BorrowingSerializer,
+    CreateBorrowingSerializer,
+)
 
 
-class BorrowingsListRetrieveAPIView(
-    mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet
+class BorrowingsAPIView(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet,
 ):
     queryset = Borrowing.objects.select_related("book", "user")
     serializer_class = BorrowingSerializer
     permission_classes = (permissions.IsAuthenticated,)
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return CreateBorrowingSerializer
+
+        return BorrowingSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
