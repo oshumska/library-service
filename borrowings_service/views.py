a5e3a7e9a5dbf6ccf22e5131a0472a0ca8a56708
Olha Shumska <olgashumska65@gmail.com>
2025-09-12 08:45:21 +0200

    create: action for returning borrowing

--- borrowings_service/views.py
+++ borrowings_service/views.py
@@ -1,11 +1,14 @@
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema, OpenApiParameter
-from rest_framework import viewsets, mixins, permissions
+from rest_framework import viewsets, mixins, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from borrowings_service.models import Borrowing
 from borrowings_service.serializers import (
     BorrowingSerializer,
     CreateBorrowingSerializer,
+    ReturnBorrowingSerializer,
 )
 
 
@@ -40,11 +43,27 @@ class BorrowingsAPIView(
     def get_serializer_class(self):
         if self.action == "create":
             return CreateBorrowingSerializer
+        elif self.action == "return_borrowing":
+            return ReturnBorrowingSerializer
 
         return BorrowingSerializer
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @action(methods=["POST"], detail=True, url_path="return")
+    def return_borrowing(self, request, pk=None):
+        borrowing = self.get_object()
+        if borrowing.user.id != self.request.user.id:
+            return Response(
+                "User can only return their own borrowings",
+                status=status.HTTP_403_FORBIDDEN,
+            )
+        serializer = self.get_serializer(borrowing, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @extend_schema(
         parameters=[
