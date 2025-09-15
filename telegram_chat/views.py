1099d8aedcb43fa8cfeede40d35170337b066875
Olha Shumska <olgashumska65@gmail.com>
2025-09-15 21:26:38 +0200

    create setwebhook function for setting webhook for bot

--- telegram_chat/views.py
+++ telegram_chat/views.py
@@ -1,9 +1,21 @@
 import requests
+from rest_framework import status
+from rest_framework.decorators import permission_classes, api_view
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 
 from library_service.settings import (
     TELEGRAM_API_URL,
     BASE_CHAT_ID,
+    NGROK_URL,
 )
+
+
+@api_view(["GET"])
+@permission_classes([IsAdminUser])
+def setwebhook(request):
+    response = requests.post(TELEGRAM_API_URL + "setWebhook?url=" + NGROK_URL).json()
+    return Response(response, status=status.HTTP_200_OK)
 
 
 def send_message_to_chat(message: str):
