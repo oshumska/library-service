b4ee4794eca5c5541556ac0efbf19ee973b4197f
Olha Shumska <olgashumska65@gmail.com>
2025-09-15 21:43:07 +0200

    add telegram_bot to urls

--- telegram_chat/urls.py
+++ telegram_chat/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from telegram_chat.views import setwebhook
+from telegram_chat.views import setwebhook, telegram_bot
 
 urlpatterns = [
     path("setwebhook/", setwebhook, name="set_webhook"),
+    path("getpost/", telegram_bot, name="telegram_bot"),
 ]
 
 app_name = "telegram-chat"
