53b9f80d5244de5b97079b0976664e4c6a48644f
Olha Shumska <olgashumska65@gmail.com>
2025-09-15 10:31:59 +0200

    create send message function and add requests to requirements

--- telegram_chat/views.py
+++ telegram_chat/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+import requests
 
-# Create your views here.
+from library_service.settings import TELEGRAM_API_URL, BASE_CHAT_ID
+
+
+def send_message_to_chat(message: str):
+    payload = {
+        "chat_id": BASE_CHAT_ID,
+        "text": message,
+        "parse_mode": "HTML",
+    }
+
+    requests.post(TELEGRAM_API_URL + "sendMessage", data=payload)
