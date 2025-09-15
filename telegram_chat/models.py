c76e926a76dddee0a6d3a6749d2f934bbba33ec3
Olha Shumska <olgashumska65@gmail.com>
2025-09-15 21:14:59 +0200

    create TelegramUser to store information about chat_id

--- telegram_chat/models.py
+++ telegram_chat/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class TelegramUser(models.Model):
+
+    chat_id = models.BigIntegerField(unique=True)
+    user = models.OneToOneField(
+        get_user_model(),
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="telegram",
+    )
