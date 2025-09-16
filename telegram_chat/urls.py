eb29b62ad363cd0aaf2a7c65ef3ebd2126bf4de9
Olha Shumska <olgashumska65@gmail.com>
2025-09-16 09:48:16 +0200

    create path for get_telegram_link

--- telegram_chat/urls.py
+++ telegram_chat/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from telegram_chat.views import setwebhook, telegram_bot
+from telegram_chat.views import setwebhook, telegram_bot, get_telegram_link
 
 urlpatterns = [
     path("setwebhook/", setwebhook, name="set_webhook"),
     path("getpost/", telegram_bot, name="telegram_bot"),
+    path("links/", get_telegram_link, name="telegram_links"),
 ]
 
 app_name = "telegram-chat"
