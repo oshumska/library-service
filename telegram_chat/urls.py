efc1d54afa115468dced9660a91e58a01105b015
Olha Shumska <olgashumska65@gmail.com>
2025-09-16 09:52:39 +0200

    add delete_webhook to path modify path for set webhook

--- telegram_chat/urls.py
+++ telegram_chat/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
-from telegram_chat.views import setwebhook, telegram_bot, get_telegram_link
+from telegram_chat.views import (
+    setwebhook,
+    telegram_bot,
+    get_telegram_link,
+    delete_webhook,
+)
 
 urlpatterns = [
-    path("setwebhook/", setwebhook, name="set_webhook"),
+    path("set/webhook/", setwebhook, name="set_webhook"),
     path("getpost/", telegram_bot, name="telegram_bot"),
     path("links/", get_telegram_link, name="telegram_links"),
+    path("delete/webhook/", delete_webhook, name="delete_webhook"),
 ]
 
 app_name = "telegram-chat"
