e786a7d30f746422c44bb39edd14a90c56485aa9
Olha Shumska <olgashumska65@gmail.com>
2025-09-17 11:01:25 +0200

    add test for telegram links page

--- telegram_chat/tests.py
+++ telegram_chat/tests.py
@@ -1,6 +1,7 @@
 import json
 from unittest.mock import patch
 
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
@@ -11,6 +12,7 @@ from telegram_chat.bot import application
 from telegram_chat.views import send_message_to_chat, send_private_message
 
 GETPOST_URL = reverse("telegram-chat:telegram_bot")
+TELEGRAM_LINK_URL = reverse("telegram-chat:telegram_links")
 
 
 class TelegramWebhookTest(TestCase):
@@ -69,3 +71,33 @@ class TestSendMessages(TestCase):
         """test function actualy send message to private chat"""
         res = send_private_message("hi from private message", BASE_CHAT_ID)
         self.assertEqual(res.status_code, 200)
+
+
+class TestTelegramLinkPublic(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        res = self.client.get(TELEGRAM_LINK_URL)
+        self.assertEqual(res.status_code, 401)
+
+
+class TestTelegramLinkPrivate(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create(
+            email="test@test.com", password="<PASSWORD>"
+        )
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_telegram_link(self):
+        res = self.client.get(TELEGRAM_LINK_URL)
+        self.assertEqual(res.status_code, 200)
+        self.assertIn("Library public chat link", res.data)
+        self.assertIn("Bot link", res.data)
+        self.assertIn(
+            "Send command to private chat with bot to get  personal notification",
+            res.data,
+        )
