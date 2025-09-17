d8a77280361143d833eec5473073e8c179b13453
Olha Shumska <olgashumska65@gmail.com>
2025-09-17 09:16:14 +0200

    add: test for telegram webhook

--- telegram_chat/tests.py
+++ telegram_chat/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase
+from rest_framework.reverse import reverse
+from rest_framework.test import APIClient
+
+
+GETPOST_URL = reverse("telegram-chat:telegram_bot")
+
+
+class TelegramWebhookTest(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.mock_update = {
+            "update_id": 123456789,
+            "message": {
+                "message_id": 1,
+                "from": {
+                    "id": 12345678,
+                    "is_bot": False,
+                    "first_name": "Test",
+                    "language_code": "uk",
+                },
+                "chat": {"id": 12345678, "first_name": "Test", "type": "private"},
+                "date": 1758041111,
+                "text": "Hello, bot",
+            },
+        }
+
+    def test_telegram_webhook_post(self):
+
+        res = self.client.post(
+            GETPOST_URL,
+            data=json.dumps(self.mock_update),
+            content_type="application/json",
+        )
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.json(), {"ok": True})
+
+    def test_telegram_webhook_get(self):
+
+        res = self.client.get(GETPOST_URL)
+        self.assertEqual(res.status_code, 400)
