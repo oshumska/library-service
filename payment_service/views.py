da97ef3f6c1e82be29cd7be46c1381ede53668b2
Olha Shumska <olgashumska65@gmail.com>
2025-09-21 14:42:07 +0200

    create: viewset for payment, override get_queryset function so non staff users see only their payments

--- payment_service/views.py
+++ payment_service/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins, permissions
 
-# Create your views here.
+from payment_service.models import Payment
+from payment_service.serializers import PaymentListSerializer, PaymentDetailSerializer
+
+
+class PaymentViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.ModelViewSet
+):
+    queryset = Payment.objects.select_related("borrowing")
+    serializer_class = PaymentListSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if self.request.user.is_staff:
+            return queryset
+        else:
+            return queryset.filter(borrowing__user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return PaymentDetailSerializer
+
+        return PaymentListSerializer
