d653c26c8d24cbd1862891aa6b2a3ae425ccaffb
Olha Shumska <olgashumska65@gmail.com>
2025-09-21 11:48:30 +0200

    modify setting.py according to README.md file

--- library_service/settings.py
+++ library_service/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["f5389634a109.ngrok-free.app", "127.0.0.1"]
+ALLOWED_HOSTS = ["your-ngrok-host", "127.0.0.1"]
 
 
 # Application definition
