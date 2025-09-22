d3c5c02321160e494abc14fd523a1d5eb305a4d2
Olha Shumska <olgashumska65@gmail.com>
2025-09-22 09:31:04 +0200

    create view to create test session

--- payment_service/views.py
+++ payment_service/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
-from rest_framework import viewsets, mixins, permissions
+import stripe
+from rest_framework import viewsets, mixins, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from payment_service.models import Payment
 from payment_service.serializers import PaymentListSerializer, PaymentDetailSerializer
+from library_service.settings import STRIPE_SECRET_KEY
+
+stripe.api_key = STRIPE_SECRET_KEY
 
 
 class PaymentViewSet(
@@ -25,3 +30,32 @@ class PaymentViewSet(
             return PaymentDetailSerializer
 
         return PaymentListSerializer
+
+
+class CreateStripeSessionView(
+    APIView,
+):
+    def post(self, request):
+        try:
+            session = stripe.checkout.Session.create(
+                line_items=[
+                    {
+                        "price_data": {
+                            "currency": "USD",
+                            "product_data": {"name": "book"},
+                            "unit_amount": 120,
+                        },
+                        "quantity": 1,
+                    }
+                ],
+                mode="payment",
+                success_url="http://127.0.0.1:8000/api/library/payment/success/",
+                cancel_url="http://127.0.0.1:8000/api/library/payment/cancel/",
+            )
+            payload = {
+                "session_id": session.id,
+                "session_url": session.url,
+            }
+            return Response(payload, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
