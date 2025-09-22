4254402b4f3ceed77019b92785f7b59ba21966c9
Olha Shumska <olgashumska65@gmail.com>
2025-09-22 09:31:55 +0200

    create path for creating strie session

--- payment_service/urls.py
+++ payment_service/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from payment_service.views import PaymentViewSet
+from payment_service.views import PaymentViewSet, CreateStripeSessionView
 
 router = routers.DefaultRouter()
-router.register("", PaymentViewSet)
+router.register("payments", PaymentViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("session/", CreateStripeSessionView.as_view(), name="session-url"),
 ]
 
 
