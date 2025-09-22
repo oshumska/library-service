a8816b8e8df0f6f986ead047435fcc45ad613437
Olha Shumska <olgashumska65@gmail.com>
2025-09-22 15:01:16 +0200

    style changes by black

--- payment_service/models.py
+++ payment_service/models.py
@@ -15,7 +15,9 @@ class Payment(models.Model):
         max_length=10, choices=StatusChoices.choices, default=StatusChoices.PENDING
     )
     type = models.CharField(max_length=10, choices=TypeChoices.choices)
-    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE, related_name="payments")
+    borrowing = models.ForeignKey(
+        Borrowing, on_delete=models.CASCADE, related_name="payments"
+    )
     session_url = models.URLField(max_length=500, null=True, blank=True)
     session_id = models.CharField(max_length=255, null=True, blank=True)
     money_to_pay = models.DecimalField(decimal_places=2, max_digits=10)
